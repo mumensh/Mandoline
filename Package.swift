8dc6b30be8a6924cea5798984e417eee5ec678c4
Ryan Kohl <49837148+rkohl@users.noreply.github.com>
2020-04-11 04:06:30 -0500

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "Mandoline",
+    products: [
+    .library(
+        name: "Mandoline",
+        targets: ["Mandoline"])
+    ],
+    dependencies: [
+        .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "Mandoline",
+            dependencies: ["SnapKit"],
+            path: "Mandoline/Classes"
+        ),
+    ]
+)
