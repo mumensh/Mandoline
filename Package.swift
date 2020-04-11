84edbe989d626ba3110cb59886207f3a608fbc86
Ryan Kohl <49837148+rkohl@users.noreply.github.com>
2020-04-11 04:12:02 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Mandoline",
+    platforms: [
+          .iOS(.v12),
+    ],
     products: [
     .library(
         name: "Mandoline",
